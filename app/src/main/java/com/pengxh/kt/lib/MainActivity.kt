3ee184d5b61566c4722b1e34102608f05d07fc51
pengxianhong <290677893@qq.com>
2022-06-21 21:49:17 +0800

    test(test): 将module变为library

--- app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
+++ app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.pengxh.kt.lib
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.pengxh.kt.lite.base.KotlinBaseActivity
+import com.pengxh.kt.lite.utils.BroadcastManager
+import com.pengxh.kt.lite.utils.Constant
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : KotlinBaseActivity() {
+
+    private val kTag = "MainActivity"
+    private lateinit var broadcastManager: BroadcastManager
+
+    override fun initLayoutView(): Int = R.layout.activity_main
+
+    override fun setupTopBarLayout() {
+
+    }
+
+    override fun initData() {
+        broadcastManager = BroadcastManager.obtainInstance(this)
+        broadcastManager.addAction(object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.d(
+                    kTag, "onReceive: " + intent?.getStringExtra(Constant.BROADCAST_INTENT_DATA_KEY)
+                )
+            }
+        }, "TestAction")
+    }
+
+    override fun initEvent() {
+        val urls = ArrayList<String>()
+        urls.add("https://images.pexels.com/photos/4221068/pexels-photo-4221068.jpeg?cs=srgb&dl=pexels-clive-kim-4221068.jpg&fm=jpg")
+        testButton.setOnClickListener {
+            broadcastManager.sendBroadcast("TestAction", kTag)
+        }
     }
 }
