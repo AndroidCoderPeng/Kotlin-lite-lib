307b55e57f7998bad352498a863886b7cf94e2be
Pengxh <290677893@qq.com>
2022-06-27 15:40:48 +0800

    feat(lib): 添加RetrofitFactory

--- app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
+++ app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.pengxh.kt.lib
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import com.pengxh.kt.lite.activity.BigImageActivity
 import com.pengxh.kt.lite.base.KotlinBaseActivity
-import com.pengxh.kt.lite.extensions.navigatePageTo
-import com.pengxh.kt.lite.utils.BroadcastManager
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : KotlinBaseActivity() {
 
     private val kTag = "MainActivity"
-    private lateinit var broadcastManager: BroadcastManager
 
     override fun initLayoutView(): Int = R.layout.activity_main
 
@@ -21,20 +15,12 @@ class MainActivity : KotlinBaseActivity() {
     }
 
     override fun initData() {
-        val urls = ArrayList<String>()
-        urls.add("https://images.pexels.com/photos/4221068/pexels-photo-4221068.jpeg?cs=srgb&dl=pexels-clive-kim-4221068.jpg&fm=jpg")
 
-        broadcastManager = BroadcastManager.obtainInstance(this)
-        broadcastManager.addAction(object : BroadcastReceiver() {
-            override fun onReceive(context: Context?, intent: Intent?) {
-                context?.navigatePageTo<BigImageActivity>(0, urls)
-            }
-        }, "TestAction")
     }
 
     override fun initEvent() {
         testButton.setOnClickListener {
-            broadcastManager.sendBroadcast("TestAction", kTag)
+
         }
     }
 }
---
+++ lite/src/main/java/com/pengxh/kt/lite/utils/RetrofitFactory.kt
@@ -0,0 +1,40 @@
+package com.pengxh.kt.lite.utils
+
+import android.util.Log
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.jetbrains.annotations.NotNull
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitFactory {
+    private const val kTag = "RetrofitFactory"
+    val httpClient: OkHttpClient by lazy { createHttpClient() }
+
+    inline fun <reified T> createRetrofit(httpConfig: String): T {
+        return Retrofit.Builder()
+            .baseUrl(httpConfig)
+            .addConverterFactory(ScalarsConverterFactory.create())          //字符串转换器
+            .addConverterFactory(GsonConverterFactory.create())             //Gson转换器
+            .addCallAdapterFactory(CoroutineCallAdapterFactory.invoke())    //协程请求适配器
+            .client(httpClient) //log拦截器
+            .build().create(T::class.java)
+    }
+
+    private fun createHttpClient(): OkHttpClient { //日志显示级别
+        val interceptor = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+            override fun log(@NotNull message: String) {
+                Log.d(kTag, ">>>>> $message")
+            }
+        })
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val builder = OkHttpClient.Builder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+        return builder.addInterceptor(interceptor).build()
+    }
+}
