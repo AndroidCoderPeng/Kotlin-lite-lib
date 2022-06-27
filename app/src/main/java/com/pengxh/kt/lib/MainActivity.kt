89193de8c73c970c6e72ec5fd2f908966d6e7b51
Pengxh <290677893@qq.com>
2022-06-27 18:44:58 +0800

    feat(lib): 添加RetrofitFactory

--- app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
+++ app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.pengxh.kt.lib
 
 import com.pengxh.kt.lite.base.KotlinBaseActivity
+import com.pengxh.kt.lite.widget.dialog.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -19,8 +20,82 @@ class MainActivity : KotlinBaseActivity() {
     }
 
     override fun initEvent() {
-        testButton.setOnClickListener {
+        //OK
+        testButton1.setOnClickListener {
+            AlertControlDialog.Builder()
+                .setContext(this)
+                .setTitle("")
+                .setMessage("")
+                .setNegativeButton("")
+                .setPositiveButton("")
+                .setOnDialogButtonClickListener(object :
+                    AlertControlDialog.OnDialogButtonClickListener {
+                    override fun onConfirmClick() {
 
+                    }
+
+                    override fun onCancelClick() {
+
+                    }
+                }).build().show()
+        }
+
+        //OK
+        testButton2.setOnClickListener {
+            ChangePasswordDialog.Builder()
+                .setContext(this)
+                .setOnDialogButtonClickListener(object :
+                    ChangePasswordDialog.OnDialogButtonClickListener {
+                    override fun onConfirmClick(oldPwd: String, newPwd: String) {
+
+                    }
+                }).build().show()
+        }
+
+        //OK
+        testButton3.setOnClickListener {
+            AlertInputDialog.Builder()
+                .setContext(this)
+                .setTitle("")
+                .setHintMessage("192.168.10.15")
+                .setNegativeButton("")
+                .setPositiveButton("")
+                .setOnDialogButtonClickListener(object :
+                    AlertInputDialog.OnDialogButtonClickListener {
+                    override fun onConfirmClick(value: String) {
+
+                    }
+
+                    override fun onCancelClick() {
+
+                    }
+                }).build().show()
+        }
+
+        testButton4.setOnClickListener {
+            AlertMessageDialog.Builder()
+                .setContext(this)
+                .setTitle("")
+                .setMessage("")
+                .setPositiveButton("")
+                .setOnDialogButtonClickListener(object :
+                    AlertMessageDialog.OnDialogButtonClickListener {
+                    override fun onConfirmClick() {
+
+                    }
+                }).build().show()
+        }
+
+        //OK
+        testButton5.setOnClickListener {
+            NoNetworkDialog.Builder()
+                .setContext(this)
+                .setOnDialogButtonClickListener(object :
+                    NoNetworkDialog.OnDialogButtonClickListener {
+                    override fun onButtonClick() {
+
+                    }
+                }).build().show()
         }
     }
 }
