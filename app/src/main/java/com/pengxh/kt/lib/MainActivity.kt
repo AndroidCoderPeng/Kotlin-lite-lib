a99ba91059a04922486ee5980e45e9cb49f198a1
Pengxh <290677893@qq.com>
2023-03-02 09:47:44 +0800

    feat(*): 添加RecyclerView多选列表通用适配器

--- app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
+++ app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
@@ -5,10 +5,11 @@ import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import com.pengxh.kt.lite.adapter.SingleChoiceAdapter
+import com.pengxh.kt.lite.adapter.MultipleChoiceAdapter
 import com.pengxh.kt.lite.adapter.ViewHolder
 import com.pengxh.kt.lite.base.KotlinBaseActivity
 import com.pengxh.kt.lite.extensions.readAssetsFile
+import com.pengxh.kt.lite.extensions.toJson
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -66,27 +67,60 @@ class MainActivity : KotlinBaseActivity() {
         /**
          * 单选
          * */
-        val singleChoiceAdapter = object : SingleChoiceAdapter<SampleListModel.DataModel.RowsModel>(
-            R.layout.item_select_sample_lv, models
-        ) {
-            override fun convertView(
-                viewHolder: ViewHolder, position: Int, item: SampleListModel.DataModel.RowsModel
+//        val singleChoiceAdapter = object : SingleChoiceAdapter<SampleListModel.DataModel.RowsModel>(
+//            R.layout.item_select_sample_lv, models
+//        ) {
+//            override fun convertView(
+//                viewHolder: ViewHolder, position: Int, item: SampleListModel.DataModel.RowsModel
+//            ) {
+//                viewHolder.setText(R.id.sampleNameView, "${item.sampleName}【${item.sampleModel}】")
+//                    .setText(R.id.manufacturingCodeView, "出厂编号：${item.manufacturingNo}")
+//                    .setText(R.id.sampleCodeView, "样品编号：${item.sampleNo}")
+//                    .setText(R.id.validDateView, "有效期至：${item.validDeadline}")
+//            }
+//        }
+//        recyclerView.addItemDecoration(
+//            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+//        )
+//        (recyclerView.itemAnimator as DefaultItemAnimator).supportsChangeAnimations = false
+//        recyclerView.adapter = singleChoiceAdapter
+//        singleChoiceAdapter.setOnCheckedListener(object :
+//            SingleChoiceAdapter.OnItemCheckedListener<SampleListModel.DataModel.RowsModel> {
+//            override fun onItemChecked(position: Int, t: SampleListModel.DataModel.RowsModel) {
+//                Log.d(kTag, t.id)
+//            }
+//        })
+
+        /**
+         * 多选
+         * */
+        val multipleChoiceAdapter =
+            object : MultipleChoiceAdapter<SampleListModel.DataModel.RowsModel>(
+                R.layout.item_select_sample_lv, models
             ) {
-                viewHolder.setText(R.id.sampleNameView, "${item.sampleName}【${item.sampleModel}】")
-                    .setText(R.id.manufacturingCodeView, "出厂编号：${item.manufacturingNo}")
-                    .setText(R.id.sampleCodeView, "样品编号：${item.sampleNo}")
-                    .setText(R.id.validDateView, "有效期至：${item.validDeadline}")
+                override fun convertView(
+                    viewHolder: ViewHolder, position: Int, item: SampleListModel.DataModel.RowsModel
+                ) {
+                    viewHolder.setText(
+                        R.id.sampleNameView,
+                        "${item.sampleName}【${item.sampleModel}】"
+                    )
+                        .setText(R.id.manufacturingCodeView, "出厂编号：${item.manufacturingNo}")
+                        .setText(R.id.sampleCodeView, "样品编号：${item.sampleNo}")
+                        .setText(R.id.validDateView, "有效期至：${item.validDeadline}")
+                }
             }
-        }
         recyclerView.addItemDecoration(
             DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
         )
         (recyclerView.itemAnimator as DefaultItemAnimator).supportsChangeAnimations = false
-        recyclerView.adapter = singleChoiceAdapter
-        singleChoiceAdapter.setOnCheckedListener(object :
-            SingleChoiceAdapter.OnItemCheckedListener<SampleListModel.DataModel.RowsModel> {
-            override fun onItemChecked(position: Int, t: SampleListModel.DataModel.RowsModel) {
-                Log.d(kTag, t.id)
+        recyclerView.adapter = multipleChoiceAdapter
+        multipleChoiceAdapter.setOnCheckedListener(object :
+            MultipleChoiceAdapter.OnItemCheckedListener<SampleListModel.DataModel.RowsModel> {
+            override fun onItemChecked(
+                position: Int, items: ArrayList<SampleListModel.DataModel.RowsModel>
+            ) {
+                Log.d("Casic", "MainActivity => onItemChecked: ${items.toJson()}")
             }
         })
     }
--- lite/src/main/java/com/pengxh/kt/lite/adapter/MultipleChoiceAdapter.kt
+++ lite/src/main/java/com/pengxh/kt/lite/adapter/MultipleChoiceAdapter.kt
@@ -1,19 +1,55 @@
 package com.pengxh.kt.lite.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+
 /**
  * RecyclerView多选适配器
  */
-//class MultipleChoiceAdapter(context: Context, private val dataRows: MutableList<T>) :
-//    RecyclerView.Adapter<MultipleChoiceAdapter.ItemViewHolder> {
-//
-//
-//    private var itemCheckedListener: OnItemCheckedListener? = null
-//
-//    interface OnItemCheckedListener {
-//        fun onItemChecked(view: View, items: ArrayList<T>)
-//    }
-//
-//    fun setOnCheckedListener(listener: OnItemCheckedListener?) {
-//        itemCheckedListener = listener
-//    }
-//}
+abstract class MultipleChoiceAdapter<T>(
+    @LayoutRes private val xmlResource: Int, private val dataRows: List<T>
+) : RecyclerView.Adapter<ViewHolder>() {
+
+    private val kTag = "MultipleChoiceAdapter"
+    private var multipleSelected = mutableSetOf<Int>()
+    private var selectedItems = ArrayList<T>()
+
+    override fun getItemCount(): Int = dataRows.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(xmlResource, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        convertView(holder, position, dataRows[position])
+
+        holder.itemView.isSelected = multipleSelected.contains(position)
+        holder.itemView.setOnClickListener {
+            if (multipleSelected.contains(position)) {
+                multipleSelected.remove(position)
+                selectedItems.remove(dataRows[position])
+                holder.itemView.isSelected = false
+            } else {
+                multipleSelected.add(position)
+                selectedItems.add(dataRows[position])
+                holder.itemView.isSelected = true
+            }
+
+            itemCheckedListener?.onItemChecked(position, selectedItems)
+        }
+    }
+
+    abstract fun convertView(viewHolder: ViewHolder, position: Int, item: T)
+
+    private var itemCheckedListener: OnItemCheckedListener<T>? = null
+
+    interface OnItemCheckedListener<T> {
+        fun onItemChecked(position: Int, items: ArrayList<T>)
+    }
+
+    fun setOnCheckedListener(listener: OnItemCheckedListener<T>?) {
+        itemCheckedListener = listener
+    }
+}
