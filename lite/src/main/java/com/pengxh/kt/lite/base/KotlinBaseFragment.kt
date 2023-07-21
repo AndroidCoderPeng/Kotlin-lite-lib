341b79d1c11cbb3fd173c331561d6a4073706823
pengxianhong <290677893@qq.com>
2023-07-21 20:24:16 +0800

    feat(*): 优化KotlinBaseFragment

--- lite/src/main/java/com/pengxh/kt/lite/base/KotlinBaseFragment.kt
+++ lite/src/main/java/com/pengxh/kt/lite/base/KotlinBaseFragment.kt
@@ -4,46 +4,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 
 abstract class KotlinBaseFragment : Fragment() {
 
+    lateinit var bv: View
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(initLayoutView(), container, false)
+        bv = inflater.inflate(initLayoutRes(), container, false)
+        initView(savedInstanceState)
+        setupTopBarLayout()
+        return bv
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupTopBarLayout()
-        initData(savedInstanceState)
         observeRequestState()
         initEvent()
     }
 
-    /**
-     * 初始化xml布局
-     */
-    abstract fun initLayoutView(): Int
+    @LayoutRes
+    abstract fun initLayoutRes(): Int
 
     /**
-     * 特定页面定制沉浸式状态栏
+     * 沉浸式状态栏
      */
     abstract fun setupTopBarLayout()
 
     /**
-     * 初始化默认数据
+     * 初始化布局以及控件
      */
-    abstract fun initData(savedInstanceState: Bundle?)
+    abstract fun initView(savedInstanceState: Bundle?)
 
     /**
-     * 数据请求状态监听
+     * 网络请求状态监听
      */
     abstract fun observeRequestState()
 
     /**
-     * 初始化业务逻辑
+     * 业务逻辑，按钮等事件
      */
     abstract fun initEvent()
 }
