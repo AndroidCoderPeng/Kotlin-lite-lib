0c8fce11690f2317aba15dc52241a0c3612f0951
Pengxh <290677893@qq.com>
2023-12-18 18:16:46 +0800

    fix(*): 解决无法播放音频的问题

--- app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
+++ app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.pengxh.kt.lib
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MotionEvent
+import android.widget.ImageView
+import android.widget.PopupWindow
+import android.widget.TextView
 import com.pengxh.kt.lib.databinding.ActivityMainBinding
 import com.pengxh.kt.lite.base.KotlinBaseActivity
-import com.pengxh.kt.lite.widget.SteeringWheelView
+import com.pengxh.kt.lite.extensions.createAudioFile
+import com.pengxh.kt.lite.extensions.millsToTime
+import com.pengxh.kt.lite.widget.audio.AudioPopupWindow
+import com.pengxh.kt.lite.widget.audio.AudioRecordHub
 import pub.devrel.easypermissions.EasyPermissions
+import java.io.File
 
 class MainActivity : KotlinBaseActivity<ActivityMainBinding>(),
     EasyPermissions.PermissionCallbacks {
@@ -13,9 +24,32 @@ class MainActivity : KotlinBaseActivity<ActivityMainBinding>(),
     private val kTag = "MainActivity"
     private val context = this@MainActivity
     private val permissionCode = 20231211
-    private val userPermissions = arrayOf(
-        Manifest.permission.RECORD_AUDIO
-    )
+    private val userPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            arrayOf(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_MEDIA_AUDIO
+            )
+        } else {
+            arrayOf(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.MANAGE_EXTERNAL_STORAGE
+            )
+        }
+    } else {
+        arrayOf(
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    }
+
+    private var recordHub: AudioRecordHub? = null
 
     override fun initViewBinding(): ActivityMainBinding {
         return ActivityMainBinding.inflate(layoutInflater)
@@ -26,34 +60,10 @@ class MainActivity : KotlinBaseActivity<ActivityMainBinding>(),
 
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun initOnCreate(savedInstanceState: Bundle?) {
         if (EasyPermissions.hasPermissions(this, *userPermissions)) {
-            binding.steeringWheelView.setOnWheelTouchListener(object :
-                SteeringWheelView.OnWheelTouchListener {
-                override fun onCenterClicked() {
-
-                }
-
-                override fun onLeftTurn() {
-
-                }
-
-                override fun onTopTurn() {
-
-                }
-
-                override fun onRightTurn() {
-
-                }
-
-                override fun onBottomTurn() {
-
-                }
-
-                override fun onActionTurnUp(dir: SteeringWheelView.Direction) {
-
-                }
-            })
+            initAudioRecord()
         } else {
             EasyPermissions.requestPermissions(
                 this@MainActivity,
@@ -62,6 +72,54 @@ class MainActivity : KotlinBaseActivity<ActivityMainBinding>(),
                 *userPermissions
             )
         }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initAudioRecord() {
+        recordHub = AudioRecordHub.get(this)
+
+        AudioPopupWindow.get(this).create(object : AudioPopupWindow.IAudioPopupCallback {
+            override fun onViewCreated(
+                window: PopupWindow, imageView: ImageView, textView: TextView
+            ) {
+                binding.recodeAudioButton.setOnTouchListener { v, event ->
+                    when (event.action) {
+                        MotionEvent.ACTION_DOWN -> {
+                            binding.recodeAudioButton.animate()
+                                .scaleX(0.75f).scaleY(0.75f)
+                                .setDuration(100).start()
+                            window.showAtLocation(binding.rootView, Gravity.CENTER, 0, 0)
+                            recordHub?.apply {
+                                initHub(createAudioFile())
+                                startRecord(object : AudioRecordHub.OnAudioStatusUpdateListener {
+                                    override fun onUpdate(db: Double, time: Long) {
+                                        imageView.drawable.level = (3000 + 6000 * db / 100).toInt()
+                                        textView.text = time.millsToTime()
+                                    }
+
+                                    override fun onStop(file: File?) {
+                                        if (file != null) {
+                                            binding.audioPathView.text =
+                                                "录音文件路径：\r\n${file.absolutePath}"
+                                            binding.audioPlayView.setAudioSource(file)
+                                        }
+                                    }
+                                })
+                            }
+                        }
+
+                        MotionEvent.ACTION_UP -> {
+                            recordHub?.stopRecord() //结束录音（保存录音文件）
+                            window.dismiss()
+                            binding.recodeAudioButton.animate()
+                                .scaleX(1.0f).scaleY(1.0f)
+                                .setDuration(100).start()
+                        }
+                    }
+                    true
+                }
+            }
+        })
     }
 
     override fun observeRequestState() {
@@ -81,10 +139,15 @@ class MainActivity : KotlinBaseActivity<ActivityMainBinding>(),
     }
 
     override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
-
+        initAudioRecord()
     }
 
     override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.audioPlayView.stop()
     }
 }
--- lite/src/main/java/com/pengxh/kt/lite/widget/audio/AudioPlayerView.kt
+++ lite/src/main/java/com/pengxh/kt/lite/widget/audio/AudioPlayerView.kt
@@ -1,57 +1,33 @@
 package com.pengxh.kt.lite.widget.audio
 
 import android.content.Context
-import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Handler
 import android.os.Message
 import android.util.AttributeSet
+import android.view.View
 import androidx.annotation.DrawableRes
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.res.ResourcesCompat
 import com.pengxh.kt.lite.utils.Constant
 import com.pengxh.kt.lite.utils.WeakReferenceHandler
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
 
 class AudioPlayerView(context: Context, attrs: AttributeSet) : AppCompatTextView(context, attrs),
-    Handler.Callback {
+    Handler.Callback, View.OnClickListener {
 
     private val kTag = "AudioPlayerView"
-    private val mediaPlayer by lazy { MediaPlayer() }
-    private val audioAttributes = AudioAttributes.Builder()
-        .setUsage(AudioAttributes.USAGE_UNKNOWN)
-        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-        .build()
-
-    /**
-     * 在非初始化状态下调用setDataSource  会抛出IllegalStateException异常
-     */
+    private lateinit var audioSource: File
+    private var mediaPlayer: MediaPlayer? = null
     private var hasPrepared = false
     private var index = 0
     private var weakReferenceHandler: WeakReferenceHandler
     private var animationRunnable: Runnable
 
     init {
-        mediaPlayer.setAudioAttributes(audioAttributes)
-        mediaPlayer.setOnPreparedListener {
-            hasPrepared = true
-            text = audioDuration
-        }
-        mediaPlayer.setOnErrorListener { mp, _, _ ->
-            mp.reset()
-            false
-        }
-        mediaPlayer.setOnCompletionListener { stopAnimation() }
-
-        setOnClickListener {
-            if (mediaPlayer.isPlaying) {
-                mediaPlayer.pause()
-                stopAnimation()
-            } else {
-                mediaPlayer.seekTo(0)
-                startAnimation()
-                mediaPlayer.start()
-            }
-        }
+        setOnClickListener(this)
 
         weakReferenceHandler = WeakReferenceHandler(this)
         animationRunnable = object : Runnable {
@@ -63,22 +39,54 @@ class AudioPlayerView(context: Context, attrs: AttributeSet) : AppCompatTextView
         }
     }
 
-    private val audioDuration: String
-        get() {
-            val duration = mediaPlayer.duration
-            return if (duration == -1) {
-                ""
-            } else {
-                val sec = duration / 1000
-                val m = sec / 60
-                val s = sec % 60
-                "$m:$s"
+    fun setAudioSource(file: File) {
+        audioSource = file
+        initMediaPlayer()
+    }
+
+    private fun initMediaPlayer() {
+        mediaPlayer = MediaPlayer()
+        mediaPlayer?.apply {
+            val inputStream = FileInputStream(audioSource)
+            setDataSource(inputStream.fd)
+            try {
+                prepare()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            setOnPreparedListener {
+                hasPrepared = true
+
+                text = if (duration == -1) {
+                    ""
+                } else {
+                    val sec = duration / 1000
+                    val m = sec / 60
+                    val s = sec % 60
+                    "$m:$s"
+                }
+            }
+
+            setOnCompletionListener {
+                stopAnimation()
             }
         }
+    }
 
-    fun setAudioSource(audioSource: String) {
-        mediaPlayer.setDataSource(audioSource)
-        mediaPlayer.prepare()
+    override fun onClick(v: View?) {
+        mediaPlayer?.apply {
+            if (isPlaying) {
+                stop()
+                release()
+                stopAnimation()
+                mediaPlayer = null
+            } else {
+                initMediaPlayer()
+                start()
+                startAnimation()
+            }
+        }
     }
 
     private fun startAnimation() {
@@ -98,10 +106,13 @@ class AudioPlayerView(context: Context, attrs: AttributeSet) : AppCompatTextView
         setCompoundDrawables(drawable, null, null, null)
     }
 
-    fun release() {
-        mediaPlayer.stop()
-        mediaPlayer.release()
+    fun stop() {
+        mediaPlayer?.apply {
+            stop()
+            release()
+        }
         weakReferenceHandler.removeCallbacks(animationRunnable)
+        mediaPlayer = null
     }
 
     override fun handleMessage(msg: Message): Boolean {
