12fe18b5e27e0e6217ab0554c840463b32cdb68d
Pengxh <290677893@qq.com>
2024-01-02 15:51:11 +0800

    feat(*): 开始完善各函数、工具类、方法例子

--- app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
+++ app/src/main/java/com/pengxh/kt/lib/MainActivity.kt
@@ -1,125 +1,35 @@
 package com.pengxh.kt.lib
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
-import android.view.Gravity
-import android.view.MotionEvent
-import android.widget.ImageView
-import android.widget.PopupWindow
-import android.widget.TextView
+import com.gyf.immersionbar.ImmersionBar
 import com.pengxh.kt.lib.databinding.ActivityMainBinding
 import com.pengxh.kt.lite.base.KotlinBaseActivity
-import com.pengxh.kt.lite.extensions.createAudioFile
-import com.pengxh.kt.lite.extensions.millsToTime
-import com.pengxh.kt.lite.widget.audio.AudioPopupWindow
-import com.pengxh.kt.lite.widget.audio.AudioRecordHub
-import pub.devrel.easypermissions.EasyPermissions
-import java.io.File
+import com.pengxh.kt.lite.extensions.convertColor
+import com.pengxh.kt.lite.extensions.getStatusBarHeight
 
-class MainActivity : KotlinBaseActivity<ActivityMainBinding>(),
-    EasyPermissions.PermissionCallbacks {
+class MainActivity : KotlinBaseActivity<ActivityMainBinding>() {
 
-    private val kTag = "MainActivity"
-    private val context = this@MainActivity
-    private val permissionCode = 20231211
-    private val userPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            arrayOf(
-                Manifest.permission.RECORD_AUDIO,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.MANAGE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_MEDIA_AUDIO
-            )
-        } else {
-            arrayOf(
-                Manifest.permission.RECORD_AUDIO,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.MANAGE_EXTERNAL_STORAGE
-            )
-        }
-    } else {
-        arrayOf(
-            Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        )
-    }
-
-    private var recordHub: AudioRecordHub? = null
+    private val itemNames = listOf(
+        "adapter", "annotations", "base", "callback",
+        "divider", "extensions", "utils", "vm",
+        "widget"
+    )
 
     override fun initViewBinding(): ActivityMainBinding {
         return ActivityMainBinding.inflate(layoutInflater)
     }
 
-
     override fun setupTopBarLayout() {
-
+        ImmersionBar.with(this)
+            .statusBarDarkFont(false)
+            .statusBarColorInt(R.color.mainColor.convertColor(this))
+            .init()
+        binding.rootView.setPadding(0, getStatusBarHeight(), 0, 0)
+        binding.rootView.requestLayout()
     }
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun initOnCreate(savedInstanceState: Bundle?) {
-        if (EasyPermissions.hasPermissions(this, *userPermissions)) {
-            initAudioRecord()
-        } else {
-            EasyPermissions.requestPermissions(
-                this@MainActivity,
-                resources.getString(R.string.app_name) + "需要获取相关权限",
-                permissionCode,
-                *userPermissions
-            )
-        }
-    }
 
-    @SuppressLint("ClickableViewAccessibility")
-    private fun initAudioRecord() {
-        recordHub = AudioRecordHub.get(this)
-
-        AudioPopupWindow.get(this).create(object : AudioPopupWindow.IAudioPopupCallback {
-            override fun onViewCreated(
-                window: PopupWindow, imageView: ImageView, textView: TextView
-            ) {
-                binding.recodeAudioButton.setOnTouchListener { v, event ->
-                    when (event.action) {
-                        MotionEvent.ACTION_DOWN -> {
-                            binding.recodeAudioButton.animate()
-                                .scaleX(0.75f).scaleY(0.75f)
-                                .setDuration(100).start()
-                            window.showAtLocation(binding.rootView, Gravity.CENTER, 0, 0)
-                            recordHub?.apply {
-                                initHub(createAudioFile())
-                                startRecord(object : AudioRecordHub.OnAudioStatusUpdateListener {
-                                    override fun onUpdate(db: Double, time: Long) {
-                                        imageView.drawable.level = (1000 + 60 * db).toInt()
-                                        textView.text = time.millsToTime()
-                                    }
-
-                                    override fun onStop(file: File?) {
-                                        if (file != null) {
-                                            binding.audioPathView.text =
-                                                "录音文件路径：\r\n${file.absolutePath}"
-                                            binding.audioPlayView.setAudioSource(file)
-                                        }
-                                    }
-                                })
-                            }
-                        }
-
-                        MotionEvent.ACTION_UP -> {
-                            recordHub?.stopRecord() //结束录音（保存录音文件）
-                            window.dismiss()
-                            binding.recodeAudioButton.animate()
-                                .scaleX(1.0f).scaleY(1.0f)
-                                .setDuration(100).start()
-                        }
-                    }
-                    true
-                }
-            }
-        })
     }
 
     override fun observeRequestState() {
@@ -128,26 +38,5 @@ class MainActivity : KotlinBaseActivity<ActivityMainBinding>(),
 
     override fun initEvent() {
 
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String?>, grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        //将请求结果传递EasyPermission库处理
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
-    }
-
-    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
-        initAudioRecord()
-    }
-
-    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        binding.audioPlayView.stop()
     }
 }
---
+++ app/src/main/java/com/pengxh/kt/lib/WelcomeActivity.kt
@@ -0,0 +1,66 @@
+package com.pengxh.kt.lib
+
+import android.Manifest
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pengxh.kt.lite.extensions.navigatePageTo
+import pub.devrel.easypermissions.EasyPermissions
+
+class WelcomeActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+
+    private val permissionsCode = 2024000
+    private val userPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_MEDIA_IMAGES
+        )
+    } else {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.MANAGE_EXTERNAL_STORAGE
+            )
+        } else {
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (EasyPermissions.hasPermissions(this, *userPermissions)) {
+            startMainActivity()
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                resources.getString(R.string.app_name) + "需要获取相关权限",
+                permissionsCode,
+                *userPermissions
+            )
+        }
+    }
+
+    private fun startMainActivity() {
+        navigatePageTo<MainActivity>()
+        finish()
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        startMainActivity()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {}
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //将请求结果传递EasyPermission库处理
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+}
