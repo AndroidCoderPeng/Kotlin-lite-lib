8a5f47c62895ab48fff63c9380310efa152f9c9d
Pengxh <290677893@qq.com>
2024-01-03 15:45:10 +0800

    refactor(*): 删除ByteArray.nv21ToBitmap，Camera已过时

--- lite/src/main/java/com/pengxh/kt/lite/extensions/ByteArray.kt
+++ lite/src/main/java/com/pengxh/kt/lite/extensions/ByteArray.kt
@@ -1,31 +1,5 @@
 package com.pengxh.kt.lite.extensions
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.ImageFormat
-import android.graphics.Rect
-import android.graphics.YuvImage
-import java.io.ByteArrayOutputStream
-import java.io.IOException
-
-/**
- * Camera
- */
-fun ByteArray.nv21ToBitmap(width: Int, height: Int): Bitmap? {
-    var bitmap: Bitmap? = null
-    try {
-        val image = YuvImage(this, ImageFormat.NV21, width, height, null)
-        val outputStream = ByteArrayOutputStream()
-        image.compressToJpeg(Rect(0, 0, width, height), 80, outputStream)
-        val bmp = BitmapFactory.decodeByteArray(outputStream.toByteArray(), 0, outputStream.size())
-        bitmap = bmp.rotateImage(-90)
-        outputStream.close()
-    } catch (e: IOException) {
-        e.printStackTrace()
-    }
-    return bitmap
-}
-
 /**
  * ByteArray转Hex
  * */
