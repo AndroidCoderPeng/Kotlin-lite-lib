b5c237f6d4cd5cee1301ea2e3e42070d66897fe5
pengxianhong <290677893@qq.com>
2024-04-23 20:03:17 +0800

    refactor(*): 优化RetrofitFactory，添加debug开关；优化TitleBarView，添加获取标题文字方法

--- lite/src/main/java/com/pengxh/kt/lite/utils/RetrofitFactory.kt
+++ lite/src/main/java/com/pengxh/kt/lite/utils/RetrofitFactory.kt
@@ -17,25 +17,30 @@ object RetrofitFactory {
      * @param timeout 网络请求超时时间，单位：秒
      * */
     inline fun <reified T> createRetrofit(
-        httpConfig: String, timeout: Long = Constant.HTTP_TIMEOUT
+        httpConfig: String, timeout: Long = Constant.HTTP_TIMEOUT, debug: Boolean = true
     ): T {
-        val interceptor = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
-            override fun log(message: String) {
-                Log.d(kTag, ">>>>> $message")
-            }
-        })
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
         val httpClientBuilder = OkHttpClient.Builder()
-            .addInterceptor(interceptor)
             .connectTimeout(timeout, TimeUnit.SECONDS)
             .readTimeout(timeout, TimeUnit.SECONDS)
             .writeTimeout(timeout, TimeUnit.SECONDS)
+        val httpClient = if (debug) {
+            val interceptor = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+                override fun log(message: String) {
+                    Log.d(kTag, ">>>>> $message")
+                }
+            })
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+            httpClientBuilder.addInterceptor(interceptor).build()
+        } else {
+            httpClientBuilder.build()
+        }
         return Retrofit.Builder()
             .baseUrl(httpConfig)
             .addConverterFactory(ScalarsConverterFactory.create())          //字符串转换器
             .addConverterFactory(GsonConverterFactory.create())             //Gson转换器
             .addCallAdapterFactory(CoroutineCallAdapterFactory.invoke())    //协程请求适配器
-            .client(httpClientBuilder.build()) //log拦截器
-            .build().create(T::class.java)
+            .client(httpClient)
+            .build()
+            .create(T::class.java)
     }
 }
