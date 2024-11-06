f4a4e4d3b3b97b863b34ef6682aeea81764688db
pengxianhong <290677893@qq.com>
2024-11-06 14:11:06 +0800

    优化DeleteEditText

--- lite/src/main/java/com/pengxh/kt/lite/widget/DeleteEditText.kt
+++ lite/src/main/java/com/pengxh/kt/lite/widget/DeleteEditText.kt
@@ -1,51 +1,65 @@
 package com.pengxh.kt.lite.widget
 
 import android.content.Context
-import android.graphics.drawable.Drawable
+import android.graphics.Paint
+import android.graphics.Rect
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnFocusChangeListener
-import android.view.animation.Animation
-import android.view.animation.OvershootInterpolator
-import android.view.animation.TranslateAnimation
 import androidx.appcompat.widget.AppCompatEditText
-import androidx.core.content.res.ResourcesCompat
 import com.pengxh.kt.lite.R
+import com.pengxh.kt.lite.extensions.convertDrawable
+
 
 class DeleteEditText constructor(context: Context, attrs: AttributeSet) :
     AppCompatEditText(context, attrs), OnFocusChangeListener, TextWatcher {
 
+    private val kTag = "DeleteEditText"
+    private val paint = Paint()
+    private val bounds = Rect()
+
     /**
      * 删除按钮的引用
      */
-    private var rightClearDrawable: Drawable?
+    private var clearDrawable = compoundDrawables[2]
     private var hasFocus = false
 
     init {
-        // 获取EditText的DrawableRight,假如没有设置我们就使用默认的图片,2是获得右边的图片  顺序是左上右下（0,1,2,3,）
-        rightClearDrawable = compoundDrawables[2]
-        if (rightClearDrawable == null) {
-            rightClearDrawable =
-                ResourcesCompat.getDrawable(context.resources, R.drawable.ic_edit_text_delete, null)
+        /**
+         * 获取EditText的DrawableRight
+         * 2是获得右边的图片，顺序是左上右下（0,1,2,3,）
+         * */
+        if (clearDrawable == null) {
+            clearDrawable = R.drawable.ic_edit_text_delete.convertDrawable(context)
         }
-        //设置图标大小
-        rightClearDrawable!!.setBounds(0, 0, 64, 64)
+
+        paint.textSize = textSize
+        paint.typeface = typeface
+
         setClearIconVisible(false)
         onFocusChangeListener = this
         addTextChangedListener(this)
+        //获取输入框文字的高度
+        post {
+            //定义一个默认文字适配用户自定义的EditText的文字大小
+            val tempText = "DeleteEditText"
+            paint.getTextBounds(tempText, 0, tempText.length, bounds)
+
+            //设置图标大小
+            val iconSize = (bounds.height() * 1.5).toInt()
+            Log.d(kTag, "EditeText View Text Height: ${bounds.height()}, Clear IconSize: $iconSize")
+            clearDrawable.setBounds(0, 0, iconSize, iconSize)
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (event.action == MotionEvent.ACTION_UP) {
-            if (compoundDrawables[2] != null) {
-                val touchable =
-                    event.x > width - totalPaddingRight && event.x < width - paddingRight
-                if (touchable) {
-                    this.setText("")
-                }
+            if (event.x > width - totalPaddingRight && event.x < width - paddingRight) {
+                setText("")
             }
         }
         return super.onTouchEvent(event)
@@ -65,18 +79,6 @@ class DeleteEditText constructor(context: Context, attrs: AttributeSet) :
     override fun afterTextChanged(s: Editable) {}
 
     /**
-     * 设置晃动动画
-     */
-    fun setShakeAnimation() {
-        val animation = TranslateAnimation(0f, 10f, 0f, 0f)
-        animation.interpolator = OvershootInterpolator()
-        animation.duration = 30
-        animation.repeatCount = 5
-        animation.repeatMode = Animation.REVERSE
-        startAnimation(animation)
-    }
-
-    /**
      * 当EditText焦点发生变化的时候，判断里面字符串长度设置清除图标的显示与隐藏
      */
     override fun onFocusChange(v: View, hasFocus: Boolean) {
@@ -89,9 +91,13 @@ class DeleteEditText constructor(context: Context, attrs: AttributeSet) :
     }
 
     private fun setClearIconVisible(visible: Boolean) {
-        val right = if (visible) rightClearDrawable else null
+        val rightIcon = if (visible) {
+            clearDrawable
+        } else {
+            null
+        }
         setCompoundDrawables(
-            compoundDrawables[0], compoundDrawables[1], right, compoundDrawables[3]
+            compoundDrawables[0], compoundDrawables[1], rightIcon, compoundDrawables[3]
         )
     }
 }
