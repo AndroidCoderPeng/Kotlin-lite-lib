9720196d14ec4fb7ffa10a7d2719ffc8536cbafb
pengxianhong <290677893@qq.com>
2024-11-11 14:55:40 +0800

    优化byte[]转ASCII码代码

--- lite/src/main/java/com/pengxh/kt/lite/extensions/ByteArray.kt
+++ lite/src/main/java/com/pengxh/kt/lite/extensions/ByteArray.kt
@@ -4,14 +4,9 @@ package com.pengxh.kt.lite.extensions
  * ByteArray转ASCII码
  * */
 fun ByteArray.toAsciiCode(): String {
-    //判断是否能被2整除
-    return if (this.size % 2 == 0) {
-        val builder = StringBuilder()
-        this.forEach {
-            builder.append(Char(it.toInt()))
-        }
-        builder.toString()
-    } else {
-        "Decode Error"
+    val builder = StringBuilder()
+    this.forEach {
+        builder.append(Char(it.toInt()))
     }
+    return builder.toString()
 }
